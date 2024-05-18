d722aa0852eb74beb62f1cdbee736a8465b34fde
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-19 00:49:42 +0800

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -5,3 +5,7 @@ pm2 stop initiad
 cp ~/.initia/data/priv_validator_state.json ~/.initia/priv_validator_state.json
 initiad comet unsafe-reset-all --home $HOME/.initia--keep-addr-book
 lz4 -c -d initia_snapshot.tar.lz4 | tar -x -C $HOME/.initia/data
+##恢复验证者priv_validator_state.json
+cp ~/.initia/priv_validator_state.json ~/.initia/data/priv_validator_state.json
+pm2 restart initiad
+rm -v initiad_snapshot.tar.lz4
