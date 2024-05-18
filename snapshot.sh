7a8324777dfeda4f9ce1e5ea227ac0614bb68722
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-19 00:47:31 +0800

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -1,3 +1,7 @@
 sudo apt update
 sudo apt install lz4
 wget -O initia_snapshot.tar.lz4 https://snapshot.huginn.tech/testnet/initia/initia_snapshot.tar.lz4
+pm2 stop initiad
+cp ~/.initia/data/priv_validator_state.json ~/.initia/priv_validator_state.json
+initiad comet unsafe-reset-all --home $HOME/.initia--keep-addr-book
+lz4 -c -d initia_snapshot.tar.lz4 | tar -x -C $HOME/.initia/data
