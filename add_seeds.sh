4de6970ed266734bf82d1bc6d81f93cfcdef1181
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-25 17:19:57 +0800

    Update add_seeds.sh

--- add_seeds.sh
+++ add_seeds.sh
@@ -1,4 +1,4 @@
 pm2 stop initiad
-SEEDS="cd69bcb00a6ecc1ba2b4a3465de4d4dd3e0a3db1@initia-testnet-seed.itrocket.net:51656,ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@testnet-seeds.polkachu.com:25756"
+SEEDS="cae5090c0fde1de1c9890e9139dbdda24233737b@seeds.cros-nest.com:26756,d1d43cc7c7aef715957289fd96a114ecaa7ba756@testnet-seeds.nodex.one:24510"
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/" $HOME/.initia/config/config.toml
 pm2 start initiad
