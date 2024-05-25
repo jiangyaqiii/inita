ca609fda668da485765bd50953bbb94d8c9e1244
jiangyaqiii <45758831+jiangyaqiii@users.noreply.github.com>
2024-05-25 17:11:21 +0800

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -8,5 +8,5 @@ lz4 -c -d initia_snapshot.tar.lz4 | tar -x -C $HOME/.initia/data
 ##恢复验证者priv_validator_state.json
 cp ~/.initia/priv_validator_state.json ~/.initia/data/priv_validator_state.json
 pm2 restart initiad
-rm -v initiad_snapshot.tar.lz4
+rm -rf initia_snapshot.tar.lz4
 pm2 list
